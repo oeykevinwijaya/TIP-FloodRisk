65507683086d158decf56b0307b9de02b3d88516
fishkatsu <oeykevinwijaya@gmail.com>
2024-05-04 17:14:35 +1000

    bug fixes in html, update app.py for path, update path for js and jpg in html

--- app.py
+++ app.py
@@ -3,12 +3,9 @@ import csv
 import os
 
 app = Flask(__name__)
-# Set the template folder path to the frontend directory
-app.template_folder = os.path.abspath('frontend')
 
 @app.route('/')
 def index():
-    # Update with live .py
     # Path to your CSV file
     csv_file_path = os.path.join(os.getcwd(), 'backend/data/9dforecast.csv')
     
@@ -23,6 +20,23 @@ def index():
     
     # Pass data to the template
     return render_template('weather_forecast.html', forecasts=forecasts)
+
+@app.route('/history.html')
+def history():
+    # Path to your CSV file
+    csv_file_path = os.path.join(os.getcwd(), 'backend/data/9dforecast.csv')
+    
+    # List to hold rows of data
+    forecasts = []
+    
+    # Read CSV data
+    with open(csv_file_path, mode='r') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            forecasts.append(row)
+    
+    # Pass data to the template
+    return render_template('history.html', forecasts=forecasts)
 
 if __name__ == '__main__':
     app.run(debug=True)
