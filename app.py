89ba0524e3e1db20cc83c6e7ca59d5d5f95f72cf
fishkatsu <oeykevinwijaya@gmail.com>
2024-05-04 17:59:09 +1000

    updated route for pages

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ import os
 
 app = Flask(__name__)
 
+# Route for landing page
 @app.route('/')
 def index():
-    # Path to your CSV file
+    # Path to CSV file
     csv_file_path = os.path.join(os.getcwd(), 'backend/data/9dforecast.csv')
     
     # List to hold rows of data
@@ -21,9 +22,10 @@ def index():
     # Pass data to the template
     return render_template('weather_forecast.html', forecasts=forecasts)
 
-@app.route('/history.html')
-def history():
-    # Path to your CSV file
+# Route for weather forecast page
+@app.route('/weather_forecast.html')
+def weatherforecast():
+    # Path to CSV file
     csv_file_path = os.path.join(os.getcwd(), 'backend/data/9dforecast.csv')
     
     # List to hold rows of data
@@ -36,7 +38,65 @@ def history():
             forecasts.append(row)
     
     # Pass data to the template
+    return render_template('weather_forecast.html', forecasts=forecasts)
+
+# Route for history.html
+@app.route('/history.html')
+def history():
+    # Path to CSV file
+    # csv_file_path = os.path.join(os.getcwd(), 'backend/data/9dforecast.csv')
+    
+    # List to hold rows of data
+    forecasts = []
+    
+    # Read CSV data
+    with open(csv_file_path, mode='r') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            forecasts.append(row)
+    
+    # Pass data to the template
     return render_template('history.html', forecasts=forecasts)
+
+# Route for warning.html
+@app.route ('/warning.html')
+def warning():
+    return render_template('warning.html')
+
+# Route for faq.html
+@app.route ('/faq.html')
+def faq():
+    return render_template('faq.html')
+
+# Route for noti.html
+@app.route ('/noti.html')
+def noti():
+    return render_template('noti.html')
+
+# Route for contact.html
+@app.route ('/contact.html')
+def contact():
+    return render_template('contact.html')
+
+# Route for email.html
+@app.route ('email.html')
+def email():
+    return render_template('email.html')
+
+# Route for data_management.html
+@app.route ('data_management.html')
+def email():
+    return render_template('data_management.html')
+
+# Route for email.html
+@app.route ('admin_page_alert.html')
+def email():
+    return render_template('admin_page_alert.html')
+
+# Route for email.html
+@app.route ('admin_access.html')
+def email():
+    return render_template('admin_access.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
